9bd1965ebfbef08b8b30f723307d230c6118677c
HTMLDigger <ripanbiswas007@gmail.com>
2022-02-27 21:43:56 +0530

    update
    update file.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import qrcode
-url = input('Please input your URL.')
-img = qrcode.make(f'{url}')
-img.save(f'{url}.png')
-img.show()
+# File name : QR_CodeGenerator
+# Author : HTMLDigger
+# Date : Fab 27th 2022
+# ## ############################################
+#    Test File.
+# ## ############################################
+
+
+# import qrcode
+# url = input('Please input your URL.')
+# img = qrcode.make(f'{url}')
+# img.save(f'{url}.png')
+# img.show()
+
